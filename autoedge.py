e60e6325b20662a6444fa0d8ca60e608ed3b904e
Gabriel <114437365+GabrielSAMatos@users.noreply.github.com>
2023-02-06 01:13:57 -0300

    2

--- autoedge.py
+++ autoedge.py
@@ -4,7 +4,7 @@ pyautogui.click(1232,296)
 pyautogui.write('2')
 pyautogui.click(1656,290)
 
-for i in range(35):
+for i in range(15):
     i = i + 1
     pyautogui.click(1247,116, duration=0.2)
     pyautogui.write('M')
--- criaremail.py
+++ criaremail.py
@@ -2,22 +2,22 @@ import pyautogui
 
 pyautogui.click(1322,62)# click na barra de pesquisa
 pyautogui.write('out')
-pyautogui.click(1333,126, duration=0.5)# click em pesquisar
+pyautogui.click(1333,126)# click em pesquisar
 pyautogui.click(1793,104, duration=1) # click em entrar 
 pyautogui.click(1442,525, duration=1.5) #click em Cria uma!
 pyautogui.click(1567,515, duration=1) # click em @outlook
 pyautogui.click(1570,587, duration=0.1) # click em @hotmail
 pyautogui.click(1349,533) #click na barra
-pyautogui.write('gabrielsamatos15')
+pyautogui.write('gabrielsamatos17')
 pyautogui.click(1585,644) # click em proximo
 pyautogui.click(1365,533, duration=1) # click em digitar
 pyautogui.write('G@briel7')
-pyautogui.click(1584,728, duration=0.5) # click em proximo
+pyautogui.click(1584,728) # click em proximo
 pyautogui.click(1392,578, duration=0.5) # click em nome
 pyautogui.write('Gabriel')
 pyautogui.click(1388,629, duration=0.2) # click em sobrenome
 pyautogui.write('Matos')
-pyautogui.click(1581,682, duration=0.5) # click em proximo
+pyautogui.click(1581,682) # click em proximo
 pyautogui.click(1390,601, duration=0.5) # click em dia
 pyautogui.click(1392,557, duration=0.5) # click em barra de rolagem
 pyautogui.click(1324,462, duration=0.5)# click em 26
@@ -25,4 +25,4 @@ pyautogui.click(1507,601, duration=0.5)# click em mes
 pyautogui.click(1449,858, duration=0.5)# click em novembro
 pyautogui.click(1554,601, duration=0.5)# click em ano
 pyautogui.write('1999')
-pyautogui.click(1586,744, duration=0.5)# click em próximo
+pyautogui.click(1586,744)# click em próximo
